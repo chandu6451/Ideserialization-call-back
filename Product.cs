7a371ebc6dad8e1c5e19c9e107707ef33262ea89
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 15:09:42 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace IDeserializationCallbackDemo
+{
+    [Serializable]
+    public class Product : IDeserializationCallback
+    {
+        int id;
+        public int ProdID
+        {   get { return id; } set { id = value; }  }
+
+        int price;
+        public int Price
+        {   get { return price; } set { price = value; }  }
+
+        int qty;
+        public int Quantity
+        {   get { return qty; } set { qty = value; }  }
+
+        [NonSerialized]
+        int total;
+        public int Total
+        {   get { return total; }   }
+
+        public Product(int id, int price, int qty)
+        {
+            this.id = id;
+            this.price = price;
+            this.qty = qty;
+            total = this.price * this.qty;
+        }
+
+        //public void CalculateTotal()
+        //{
+        //    this.total = this.qty * this.price;
+        //}
+
+        public void OnDeserialization(object sender)
+        {
+            this.total = this.qty * this.price;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace IDeserializationCallbackDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product(101, 30, 23);
+            FileStream fs = new FileStream("Prod.txt", FileMode.Create, FileAccess.Write);
+            BinaryFormatter bin = new BinaryFormatter();
+            bin.Serialize(fs, p);
+            fs.Close();
+
+            fs = new FileStream("Prod.txt", FileMode.Open, FileAccess.Read);
+            Product prod = (Product)bin.Deserialize(fs);
+            fs.Close();
+
+            Console.WriteLine("Product ID : " + prod.ProdID);
+            Console.WriteLine("Product Price : " + prod.Price);
+            Console.WriteLine("Product Quantity : " + prod.Quantity);
+            Console.WriteLine("Product Total : " + prod.Total);
+            
+            Console.ReadKey();
+        }
+    }
+}
